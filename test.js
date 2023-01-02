1ee2c60af0c2dfea9774b0581408cd6f568aaa3b
Daniel Langerman <langerman@outlook.com>
2023-01-02 18:22:12 -0500

    JavaScript Fundamentals: Data Types

---
+++ test.js
@@ -0,0 +1,16 @@
+// single operations
+console.log(-4 * 1.5); // => -6
+console.log(-4 + 1.0); // => -3
+
+// multiple operations
+console.log(5 * 3 + 2); // => 17
+console.log(2 + 3 * 5); // => 17
+console.log((2 + 3) * 5); // => 25
+
+// JavaSCript outputs a single value, e.g.
+console.log(1 + 1)
+console.log(2.5 - 2)
+console.log(3 * 4)
+
+// semicolon at end of line is only necessary when running multiple lines of code on one line
+
