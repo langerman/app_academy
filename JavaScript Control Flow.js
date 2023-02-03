9e06a2861d2a007ed318dfe69cbea75cf739646b
Daniel Langerman <langerman@outlook.com>
2023-02-03 12:20:17 -0500

    isOdd() conditional vs without.

--- JavaScript Control Flow.js
+++ JavaScript Control Flow.js
@@ -19,11 +19,29 @@
 
 
 
-function isFive(num) {
-    if (num === 5) {
-      return true;
-    }
-    // we don't have to wrap this in an `else` because if the above code did *not*
-    // run then the number can't be 5 so we can just return false!
-    return false
-  }
+// function isFive(num) {
+//     if (num === 5) {
+//       return true;
+//     }
+//     // we don't have to wrap this in an `else` because if the above code did *not*
+//     // run then the number can't be 5 so we can just return false!
+//     return false
+//   }
+
+
+
+// function isOdd(num) {
+//     return (num % 2 !== 0)
+//   }
+  
+//   function isOddConditional(num) {
+//       if (num % 2 !== 0) {
+//           return true
+//       }
+//       return false
+//   }
+  
+//   console.log(isOdd(2)); // => false
+//   console.log(isOdd(5)); // => true
+//   console.log(isOdd(-17)); // => true
+
