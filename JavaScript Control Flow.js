07c33492c737bdde1d94f6a69eade6f2c2bc7864
Daniel Langerman <langerman@outlook.com>
2023-02-03 12:47:51 -0500

    String.indexOf()

--- JavaScript Control Flow.js
+++ JavaScript Control Flow.js
@@ -45,3 +45,13 @@
 //   console.log(isOdd(5)); // => true
 //   console.log(isOdd(-17)); // => true
 
+function isSubstring(searchString, subString) {
+    let lowerSearchStr = searchString.toLowerCase();
+    let lowerSubStr = subString.toLowerCase();
+
+    return lowerSearchStr.indexOf(lowerSubStr) !== -1;
+}
+
+console.log(isSubstring("The cat went to the store", "he cat went")); // => true
+console.log(isSubstring("Time to program", "time")); // => true
+console.log(isSubstring("Jump for joy", "joys")); // => false
