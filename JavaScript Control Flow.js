73e85ab790af357dca0eb8892701051eb5a4f591
Daniel Langerman <langerman@outlook.com>
2023-02-06 09:39:37 -0500

    added countVowels() arguments.

--- JavaScript Control Flow.js
+++ JavaScript Control Flow.js
@@ -239,13 +239,20 @@
 
 
 
-function countVowels(word) {
-    let vowels = ["a", "e", "i", "o", "u"];
-    let count = 0;
-    for (let i = 0; i < word.length; i++) {
-        if (vowels.includes(word[i].toLowerCase())) {
-            count++;
-        }
-    }
-    return count;
-}
+// function countVowels(word) {
+//     let vowels = ["a", "e", "i", "o", "u"];
+//     let count = 0;
+//     for (let i = 0; i < word.length; i++) {
+//         if (vowels.includes(word[i].toLowerCase())) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// console.log(countVowels("bootcamp")); // => 3
+// console.log(countVowels("apple")); // => 2
+// console.log(countVowels("pizza")); // => 2
+
+
+
